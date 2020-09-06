8dc4e3ebb277adbff9bf50b795a5a19930dba8df
Najib <macbook@MacBooks-Najib.local>
2020-09-06 23:23:34 +0700

    Interactions and Animations SVG Animation Components and Props States and Events Styled Components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Desing Code React',
+    title: 'Design Code React',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,64 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card'
+import Section from '../components/Section'
+import Wave from '../components/Wave'
 
 const IndexPage = () => (
-  <div className="Hero">
-    <div className="HeroGroup">
-      <div>
-        <h1>
-          Learn to <br />
-          design and code React apps
-        </h1>
-        <p>
-          Complete courses about the best tools and design systems. Prototype
-          and build apps with React and Swift.
-        </p>
-        <Link to="/page-2/">Watch the video</Link>
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <div>
+          <h1>
+            Learn to <br />
+            design and code React apps
+          </h1>
+          <p>
+            Complete courses about the best tools and design systems. Prototype
+            and build apps with React and Swift.
+          </p>
+          <Link to="/page-2/">Watch the video</Link>
+
+          <div className="Logos">
+            <img src={require('../images/logo-sketch.png')} width="50" />
+            <img src={require('../images/logo-figma.png')} width="50" />
+            <img src={require('../images/logo-studio.png')} width="50" />
+            <img src={require('../images/logo-framer.png')} width="50" />
+            <img src={require('../images/logo-react.png')} width="50" />
+            <img src={require('../images/logo-swift.png')} width="50" />
+          </div>
+
+          <Wave />
+
+        </div>
       </div>
     </div>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+        <div className="CardGroup">
+          <Card 
+            title="DesignSystem"
+            text="10 sections"
+            image={require('../images/wallpaper.jpg')} />
+          <Card 
+            title="React for Designers"
+            text="11 sections"
+            image={require('../images/wallpaper2.jpg')} />
+          <Card 
+            title="Sound Design"
+            text="5 sections"
+            image={require('../images/wallpaper3.jpg')} />
+          <Card 
+            title="ARKit 2"
+            text="10 sections"
+            image={require('../images/wallpaper4.jpg')} />
+        </div>
+    </div>
+    <Section
+      image={require('../images/wallpaper2.jpg')}
+      logo={require('../images/logo-react.png')}
+      title="React for Designers"
+      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify." />
   </div>
 )
 
